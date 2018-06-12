88ea3af083aec58fe99a0519d486d3c7ac8d1d02
SeungChul Lee <john.lee@hardkernel.com>
2018-06-12 15:36:49 +0900

    examples/Hello_World:Add function changing text color and rotation

--- examples/Hello_World/Hello_World.ino
+++ examples/Hello_World/Hello_World.ino
@@ -1,14 +1,42 @@
 #include <odroid_go.h>
- 
+
+uint8_t idx;
+uint8_t rotate;
+
 void setup() {
   // put your setup code here, to run once:
   GO.begin();
- 
-  GO.lcd.setTextSize(2);
-  GO.lcd.print("Hello, ODROID-GO");
+
+  GO.lcd.println("Hello, ODROID-GO");
+
+  delay(1000);
 }
- 
+
+void displayGO()
+{
+  GO.lcd.clearDisplay();
+  GO.lcd.setRotation(rotate+4);
+  GO.lcd.setCursor(30, 40);
+  
+  if (idx) {
+    GO.lcd.setTextSize(1);
+    GO.lcd.setTextFont(4);
+    GO.lcd.setTextColor(MAGENTA);
+  } else {
+    GO.lcd.setTextSize(2);
+    GO.lcd.setTextFont(1);
+    GO.lcd.setTextColor(GREEN);
+  }
+  GO.lcd.print("Hello, ODROID-GO");
+  
+  idx = !idx;
+  rotate++;
+  rotate = rotate%4;
+  
+  delay(1000);
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
- 
+  displayGO();
 }
--- src/utility/Display.cpp
+++ src/utility/Display.cpp
@@ -372,7 +372,7 @@ void ILI9341::setRotation(uint8_t m) {
     _height = TFT_WIDTH;
     break;
   case 5:
-    writedata(TFT_MAD_MV | TFT_MAD_MX | TFT_MAD_BGR);
+    writedata(TFT_MAD_MX | TFT_MAD_BGR);
     _width = TFT_HEIGHT;
     _height = TFT_WIDTH;
     break;
