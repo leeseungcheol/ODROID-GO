36c7a5bfa8e41f1fbd881cbc18fe98c1ecf735a4
SeungChul Lee <john.lee@hardkernel.com>
2019-03-18 11:54:21 +0900

    exapmles: Fix the wrong code

--- examples/I2C_16x2_LCD/I2C_16x2_LCD.ino
+++ examples/I2C_16x2_LCD/I2C_16x2_LCD.ino
@@ -22,7 +22,7 @@ void setup() {
     adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_11);
     esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, DEFAULT_VREF, &adc_chars);
 
-    while (cnt > 4) {
+    while (cnt < 4) {
         lcd_addr = getAddress();
         if (lcd_addr > 0)
             break;
